f3e04a6f66bdfbaccc521ec041bfaf07901b46ff
Thoof <jason.heraldo@gmail.com>
2016-07-03 13:19:12 -0700

    Centered & resizing canvas
    Has an issue with resizing right panel. Pushes instead of resizing
    canvas after a certain point

--- 2016 Jason/Explorations/week2/1. Panel object/GUI_Scripts/view.js
+++ 2016 Jason/Explorations/week2/1. Panel object/GUI_Scripts/view.js
@@ -56,37 +56,50 @@ $( document ).ready(function() {
 	leftPanel.setFirstTabActive();
 	rightPanel.setFirstTabActive();
 	
-
-	//Silly canvas resize in center
-	/*$( window ).resize(function() {
-		var initWidth = 640.0;
-		var initHeight = 480.0;
-		var panelWidth = 234;
-		
-		// Vars to resize the GLCanvas with
-		var availableWidth = $(window).width() - (2 * panelWidth);
-		var availableHeight = $(window).height() - 48 - panelWidth;
-		var width = availableWidth;
-		var height = availableHeight;
-		
-		// Resize to the dimension with the lowest proportion
-		if (availableWidth / initWidth > availableHeight / initHeight) {
-			width = (height / initHeight) * initWidth;  // Resize based on height
-		} else {
-			height = (width / initWidth) * initHeight;  // Resize based on width
-		}
-
-		// Set the proper width and height
-		$('#GLCanvas').width(width);
-		$('#GLCanvas').height(height);
-		
-		// Keep it centered (note: 219 and 229 are the w/h of the left/bottom panels including their white space)
-		$('#GLCanvasDiv').css('position', 'absolute');
-		$('#GLCanvasDiv').css("top", Math.max(0, (($(window).height() - $('#GLCanvasDiv').outerHeight()) / 2) + 
-                                                $(window).scrollTop()) + "px");
-		$('#GLCanvasDiv').css("left", Math.max(0, (($(window).width() - $('#GLCanvasDiv').outerWidth()) / 2) + 
-                                                $(window).scrollLeft()) + "px");
-	});*/
+	onWindowResize();
+	
+	$( window ).resize(function() {
+		onWindowResize();
+	});
 	
 });
+
+var onWindowResize = function() {
+	var initWidth = 640.0;
+	var initHeight = 480.0;
+	
+	var leftPanelEdge = (($("#panelLeft").offset().left + $("#panelLeft").outerWidth()));
+	var rightPanelEdge = $("#panelRight").position().left;
+	
+	var dropdownEdge = 48;
+	var bottomPanelEdge = $("#panelBottom").position().top;
+
+	// Vars to resize the GLCanvas with
+	var availableWidth = rightPanelEdge - leftPanelEdge;
+	var availableHeight = bottomPanelEdge - dropdownEdge;
+	var width = availableWidth;
+	var height = availableHeight;
+	
+	// Resize to the dimension with the lowest proportion
+	if (availableWidth / initWidth > availableHeight / initHeight) {
+		width = (height / initHeight) * initWidth;  // Resize based on height
+	} else {
+		height = (width / initWidth) * initHeight;  // Resize based on width
+	}
+
+	// Set the proper width and height
+	$('#GLCanvas').width(width);
+	$('#GLCanvas').height(height);
+	
+	
+	//console.log (rightPanelEdge - leftPanelEdge);
+	var centerX = ((rightPanelEdge - leftPanelEdge) / 2) + leftPanelEdge - (width / 2);
+	console.log(rightPanelEdge);
+	
+	
+	// Keep it centered (note: 219 and 229 are the w/h of the left/bottom panels including their white space)
+	$('#GLCanvasDiv').css('position', 'absolute');
+	$('#GLCanvasDiv').css("top", dropdownEdge + "px");
+	$('#GLCanvasDiv').css("left", centerX + "px");
+};
 
