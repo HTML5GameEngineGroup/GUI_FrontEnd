4a852fcfe1f9791b174b83c7af47d21f8be91bd7
Thoof <jason.heraldo@gmail.com>
2016-07-09 10:55:25 -0700

    Move onwindowresize to view

--- 2016 Jason/Explorations/week2/1. Panel object/GUI_Scripts/GuiBase/GuiBase_View.js
+++ 2016 Jason/Explorations/week2/1. Panel object/GUI_Scripts/GuiBase/GuiBase_View.js
@@ -584,6 +584,55 @@ gGuiBase.View = (function() {
 		}
 		return colors;
 	};
+	
+	var onWindowResize = function() {
+		var initWidth = 640.0;
+		var initHeight = 480.0;
+		
+		//var tabWidth = gGuiBase.View.getTabsWidth("#panelLeft");
+		//var leftPanelEdge = Math.max($("#panelLeft").outerWidth(true), tabWidth);
+		var leftPanelEdge = $("#panelLeft").outerWidth(true);
+		
+		//var tabWidth = gGuiBase.View.getTabsWidth("#panelRight");
+		//var rightEdge = Math.max($("#panelRight").outerWidth(true), tabWidth);
+		//var rightPanelEdge = $(window).width() - rightEdge;
+		var rightPanelEdge = $(window).width() - $("#panelRight").outerWidth(true);
+		
+		var dropdownEdge = 48;
+		var bottomPanelEdge = $("#panelBottom").position().top;
+
+		// Vars to resize the GLCanvas with
+		var availableWidth = rightPanelEdge - leftPanelEdge;
+		var availableHeight = bottomPanelEdge - dropdownEdge;
+		var width = availableWidth;
+		var height = availableHeight;
+		
+		// Resize to the dimension with the lowest proportion
+		if (availableWidth / initWidth > availableHeight / initHeight) {
+			width = (height / initHeight) * initWidth;  // Resize based on height
+		} else {
+			height = (width / initWidth) * initHeight;  // Resize based on width
+		}
+
+		// Set the proper width and height
+		$('#GLCanvas').width(width);
+		$('#GLCanvas').height(height);
+		
+		
+		//console.log (rightPanelEdge - leftPanelEdge);
+		var centerX = ((rightPanelEdge - leftPanelEdge) / 2) + leftPanelEdge - (width / 2);
+		var centerY = ((bottomPanelEdge - dropdownEdge) / 2) + dropdownEdge - (height / 2);
+
+		
+		// Keep it centered (note: 219 and 229 are the w/h of the left/bottom panels including their white space)
+		$('#GLCanvasDiv').css('position', 'absolute');
+		$('#GLCanvasDiv').css("top", centerY + "px");
+		$('#GLCanvasDiv').css("left", centerX + "px");
+	};
+
+
+
+
 
 	//Public functions and variables
 	var mPublic = {
@@ -605,6 +654,7 @@ gGuiBase.View = (function() {
 		getTabsWidth: getTabsWidth,
 		CONTENT_STYLE: CONTENT_STYLE,
 		colorStringToRGBA: colorStringToRGBA,
+		onWindowResize: onWindowResize,
 		
 		inheritPrototype: inheritPrototype
     };
--- 2016 Jason/Explorations/week2/1. Panel object/GUI_Scripts/GuiBase/view.js
+++ 2016 Jason/Explorations/week2/1. Panel object/GUI_Scripts/GuiBase/view.js
@@ -69,60 +69,13 @@ $( document ).ready(function() {
 	leftPanel.setFirstTabActive();
 	rightPanel.setFirstTabActive();
 	
-	onWindowResize();
+	gGuiBase.View.onWindowResize();
 	gGuiBase.Core.initializeInitialScene();
 	
 	$( window ).resize(function() {
-		onWindowResize();
+		gGuiBase.View.onWindowResize();
 		
 	});
 	
 });
-
-var onWindowResize = function() {
-	var initWidth = 640.0;
-	var initHeight = 480.0;
-	
-	//var tabWidth = gGuiBase.View.getTabsWidth("#panelLeft");
-	//var leftPanelEdge = Math.max($("#panelLeft").outerWidth(true), tabWidth);
-	var leftPanelEdge = $("#panelLeft").outerWidth(true);
-	
-	//var tabWidth = gGuiBase.View.getTabsWidth("#panelRight");
-	//var rightEdge = Math.max($("#panelRight").outerWidth(true), tabWidth);
-	//var rightPanelEdge = $(window).width() - rightEdge;
-	var rightPanelEdge = $(window).width() - $("#panelRight").outerWidth(true);
-	
-	var dropdownEdge = 48;
-	var bottomPanelEdge = $("#panelBottom").position().top;
-
-	// Vars to resize the GLCanvas with
-	var availableWidth = rightPanelEdge - leftPanelEdge;
-	var availableHeight = bottomPanelEdge - dropdownEdge;
-	var width = availableWidth;
-	var height = availableHeight;
-	
-	// Resize to the dimension with the lowest proportion
-	if (availableWidth / initWidth > availableHeight / initHeight) {
-		width = (height / initHeight) * initWidth;  // Resize based on height
-	} else {
-		height = (width / initWidth) * initHeight;  // Resize based on width
-	}
-
-	// Set the proper width and height
-	$('#GLCanvas').width(width);
-	$('#GLCanvas').height(height);
-	
-	
-	//console.log (rightPanelEdge - leftPanelEdge);
-	var centerX = ((rightPanelEdge - leftPanelEdge) / 2) + leftPanelEdge - (width / 2);
-	var centerY = ((bottomPanelEdge - dropdownEdge) / 2) + dropdownEdge - (height / 2);
-
-	
-	// Keep it centered (note: 219 and 229 are the w/h of the left/bottom panels including their white space)
-	$('#GLCanvasDiv').css('position', 'absolute');
-	$('#GLCanvasDiv').css("top", centerY + "px");
-	$('#GLCanvasDiv').css("left", centerX + "px");
-};
-
-
 
